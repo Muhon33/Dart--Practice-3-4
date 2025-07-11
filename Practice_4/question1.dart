35e2316e14b6efa79ad71c9f889fae47d4b4847f
Mahbub Muhon <121685498+Muhon33@users.noreply.github.com>
2025-07-11 23:55:19 +0600

    Update question1.dart

--- Practice_4/question1.dart
+++ Practice_4/question1.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<String> names = ["Goku", "Luffy", "Kurosaki", "Naruto"];
+  List<String> names = ["Ben", "Gwen", "Kevin", "Henry"];
   for (int i = 0; i < names.length; i++) {
     print(names[i]);
   }
