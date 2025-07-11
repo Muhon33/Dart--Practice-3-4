d3b033e3132d9bd172cea7ef73ca7f684896cf01
Mahbub Muhon <121685498+Muhon33@users.noreply.github.com>
2025-07-11 23:57:00 +0600

    Update question3.dart

--- Practice_4/question3.dart
+++ Practice_4/question3.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   List<double> expenses = [];
   double total = 0;
-  print("Enter the number of expenses:");
+  print("Number of expenses:");
   int? n = int.parse(stdin.readLineSync()!);
   for (int i = 0; i < n; i++) {
     double amount = double.parse(stdin.readLineSync()!);
